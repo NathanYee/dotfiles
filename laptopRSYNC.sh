3500c1dae9c096ad655d6d26a8d8e70f1894654b
nathanyee <hooticambo@gmail.com>
2019-07-05 20:49:35 -0400

    updated laptop rsync

--- laptopRSYNC.sh
+++ laptopRSYNC.sh
@@ -1,3 +1,3 @@
-rsync -av ~/.config/i3/ i3/
-#rsync -av ~/.tmux.conf .
-#rsync -av ~/.config/nvim nvim
+rsync -av ~/.config/i3/ .config/i3/
+rsync -av ~/.tmux.conf .
+rsync -av ~/.config/nvim/ .config/nvim/
