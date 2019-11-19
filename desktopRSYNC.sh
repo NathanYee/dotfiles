fef51e54fcb0b8772e3fe79f448331b7babde839
nathanyee <hooticambo@gmail.com>
2019-11-19 11:35:48 -0500

    now syncing gtk-3.0

--- desktopRSYNC.sh
+++ desktopRSYNC.sh
@@ -3,3 +3,4 @@ rsync -av ~/.config/i3/ desktop/.config/i3/
 rsync -av ~/.tmux.conf desktop/
 rsync -av ~/.config/nvim desktop/nvim
 rsync -av ~/.config/fish desktop/fish
+rsync -av ~/.config/gtk-3.0 desktop/gtk-3.0
