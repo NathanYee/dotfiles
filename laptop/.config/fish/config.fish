81ae318b883e1856df937b9494cf12e916412267
nathanyee <hooticambo@gmail.com>
2019-11-22 14:36:04 -0500

    sync laptop dotfiles

--- laptop/.config/fish/config.fish
+++ laptop/.config/fish/config.fish
@@ -5,5 +5,7 @@ eval (eval /home/nathan/anaconda3/bin/conda "shell.fish" "hook" $argv)
 # <<< conda initialize <<<
 export PATH="/home/nathan/anaconda3/bin:$PATH"
 source /home/nathan/anaconda3/etc/fish/conf.d/conda.fish
+set -gx EDITOR vim
 
 set fish_greeting
+alias vpnui="/opt/cisco/anyconnect/bin/vpnui"
--- laptopRSYNC.sh
+++ laptopRSYNC.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-rsync -av ~/.config/i3/ laptop/.config/i3/
-rsync -av ~/.tmux.conf laptop/.
-rsync -av ~/.config/nvim/ laptop/.config/nvim/
-rsync -av ~/.config/fish desktop/fish
+rsync -av ~/.config/i3/ laptop/.config/i3
+rsync -av ~/.tmux.conf laptop/
+rsync -av ~/.config/nvim/ laptop/.config/nvim
+rsync -av ~/.config/fish/ laptop/.config/fish
+rsync -av ~/.config/gtk-3.0/ laptop/.config/gtk-3.0
