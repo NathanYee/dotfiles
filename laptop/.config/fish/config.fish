86a1a016531c94fbfb7e707b9980392abc698248
nathanyee <hooticambo@gmail.com>
2020-01-10 15:26:05 -0500

    updated laptopX

--- laptop/.config/fish/config.fish
+++ laptop/.config/fish/config.fish
@@ -1,11 +1,9 @@
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval (eval /home/nathan/anaconda3/bin/conda "shell.fish" "hook" $argv)
+eval (eval /home/nathan/miniconda3/bin/conda "shell.fish" "hook" $argv)
 # <<< conda initialize <<<
-export PATH="/home/nathan/anaconda3/bin:$PATH"
-source /home/nathan/anaconda3/etc/fish/conf.d/conda.fish
-set -gx EDITOR vim
+export PATH="/home/nathan/miniconda3/bin:$PATH"
+source /home/nathan/miniconda3/etc/fish/conf.d/conda.fish
 
 set fish_greeting
-alias vpnui="/opt/cisco/anyconnect/bin/vpnui"
