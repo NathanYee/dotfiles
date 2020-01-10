e68b44c9e85660e7d7ef8468ddb3aab266c3dd06
nathanyee <hooticambo@gmail.com>
2020-01-10 15:24:50 -0500

    now syning .XCompose and .XResources

--- laptopRSYNC.sh
+++ laptopRSYNC.sh
@@ -4,3 +4,5 @@ rsync -av ~/.tmux.conf laptop/
 rsync -av ~/.config/nvim/ laptop/.config/nvim
 rsync -av ~/.config/fish/ laptop/.config/fish
 rsync -av ~/.config/gtk-3.0/ laptop/.config/gtk-3.0
+rsync -av ~/.XCompose laptop/
+rsync -av ~/.Xresources laptop/
