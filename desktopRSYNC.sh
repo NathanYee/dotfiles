2fdacb7b927c109bdf54dbe1b8c644d8aad9b304
nathanyee <hooticambo@gmail.com>
2020-06-06 13:31:45 -0400

    small updates before reset

--- desktopRSYNC.sh
+++ desktopRSYNC.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 rsync -av ~/.config/i3/ desktop/.config/i3
 rsync -av ~/.tmux.conf desktop/
+rsync -av ~/.imwheelrc desktop/
 rsync -av ~/.config/nvim/ desktop/.config/nvim
 rsync -av ~/.config/fish/ desktop/.config/fish
 rsync -av ~/.config/gtk-3.0/ desktop/.config/gtk-3.0
