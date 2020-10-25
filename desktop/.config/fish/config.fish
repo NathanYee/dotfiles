014b02d891613ff753eef3bf0da88233f4477447
nathanyee <hooticambo@gmail.com>
2020-10-25 13:45:37 -0400

    better LSP management in nvim

--- desktop/.config/fish/config.fish
+++ desktop/.config/fish/config.fish
@@ -8,6 +8,11 @@ export PATH="/home/nathan/miniconda3/bin:$PATH"
 #export PATH="/home/nathan/git/sourcekit-lsp:$PATH"
 source /home/nathan/miniconda3/etc/fish/conf.d/conda.fish
 export PATH="/home/nathan/.cargo/bin:$PATH"
+export CUDA_PATH="/usr/local/cuda"
+export GOBIN="/home/nathan/d/go/bin"
+export PATH="$GOBIN:$PATH"
+
+eval (direnv hook fish)
 
 set fish_greeting
 
