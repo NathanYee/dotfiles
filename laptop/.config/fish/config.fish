af2ec88341a39d8d811cc7c70bebb0c6f838ca4b
nathanyee <hooticambo@gmail.com>
2020-12-03 09:26:24 -0800

    laptop sync

--- laptop/.config/fish/config.fish
+++ laptop/.config/fish/config.fish
@@ -6,5 +6,8 @@ eval (eval /home/nathan/miniconda3/bin/conda "shell.fish" "hook" $argv)
 export PATH="/home/nathan/miniconda3/bin:$PATH"
 export PATH="/home/nathan/.cargo/bin:$PATH"
 source /home/nathan/miniconda3/etc/fish/conf.d/conda.fish
+export GOBIN="/home/nathan/d/go/bin"
+export PATH="$GOBIN:$PATH"
+eval (direnv hook fish)
 
 set fish_greeting
