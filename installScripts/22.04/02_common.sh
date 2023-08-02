8a01ca286c9e7b92ca18689f1a681c29b5de2e2f
nathanyee <hooticambo@gmail.com>
2023-08-02 16:10:03 -0700

    added some more common programs

--- installScripts/22.04/02_common.sh
+++ installScripts/22.04/02_common.sh
@@ -6,7 +6,8 @@ sudo apt-get install -y \
     wget curl unzip rsync renameutils ncdu gpg apt-transport-https \
     okular calibre qbittorrent \
     pulseaudio pavucontrol alsa-base \
-    i3 rofi lxappearance i3blocks i3lock arandr feh imwheel
+    i3 rofi lxappearance i3blocks i3lock arandr feh imwheel \
+    graphviz vlc ffmpeg flameshot gparted
 
 echo ""
 echo "Done installing common programs"
---
+++ installScripts/22.04/09_i3.sh
@@ -0,0 +1,6 @@
+
+mkdir ~/.themes
+cd ~/Downloads
+
+git clone https://github.com/bennetthardwick/gruvbox-gtk.git || true
+mv gruvbox-gtk ~/.themes/
